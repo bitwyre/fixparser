e8db3609c034cb9e276db591fc2f5637d0343ce3
Sonkeng Maldini <sdmg15@pm.me>
2019-12-13 16:06:31 +0100

    chore: refactoring using data types and template magic

--- src/fixparser.hpp
+++ src/fixparser.hpp
@@ -1,4 +1,5 @@
 #include <string>
+#include <cstring>
 #include <string_view> 
 #include <iostream> 
 #include <vector>
@@ -6,6 +7,7 @@
 #include <filesystem>
 #include <sstream>
 #include <algorithm>
+#include <type_traits> 
 #include <pugixml.hpp>
 
 #define SOH '|'
@@ -13,148 +15,234 @@
 namespace fixparser {
 
 namespace fs = std::filesystem;
-using FixMessage = std::string;
+// Defining the necessary types to hold a FIX message 
+struct Field{
+    std::string fieldName_;
+    std::string value_;
+    std::uint16_t number_{};
+    bool isRequired_{};
+    bool isComponent_{};
+    bool isInGroup_{};
+};
 
-    enum class FixStd : char {
-        FIX44
-    };
+struct Group{
+    std::string groupName_;
+    bool isRequired_{};
+    std::vector<Field> grpFields_;
+};
 
-    std::vector<std::pair<pugi::xml_node,std::string>> header_;
-    std::vector<std::pair<pugi::xml_node,std::string>> body_;
-    std::vector<std::pair<pugi::xml_node,std::string>> trailer_;
+struct Component{
+    Group group_;
+};
+struct Message{
+    std::string msgName_;
+    std::string msgType_;
+    std::string msgCat_;
+    std::vector<Field> msgFields_;
+}; 
 
-    [[nodiscard]] auto split(const std::string s, const char delimiter) noexcept -> std::vector<std::string> {
-        
-        std::vector<std::string> internal;
-        std::stringstream ss(s); // Turn the string into a stream.
-        std::string tok;
-        
-        while(getline(ss, tok, delimiter)) {
-            internal.emplace_back(tok);
-        }
-        
-        return internal;
-    }
- 
-    auto categorize(const pugi::xml_node& node, std::string_view nodeValue, const pugi::xml_document& fixSpec) noexcept -> void {
+struct Value{
+    std::string enumValue_;
+    std::string description_;
+};
 
-        auto headers = fixSpec.child("fix").child("header");
-        auto trailers = fixSpec.child("fix").child("trailer");
+struct Tag{
+    std::uint16_t number_{};
+    std::string name_;
+    std::string type_;
+    std::string value_;
+    std::vector<Value> tagValues_;
+};
 
-        bool foundInTrailer = false;
-        auto foundInHeader = headers.find_child_by_attribute("field","name",node.attribute("name").as_string());
+struct Header{
+    std::vector<Field> headerFields_;
+    Group grp_;
+};
 
-        if(!foundInHeader){
-            foundInTrailer = trailers.find_child_by_attribute("field","name",node.attribute("name").as_string());
+struct Body{
+    std::vector<Tag> tagValues_;
+};
 
-            if( foundInTrailer ){
-                trailer_.emplace_back( std::make_pair(node, nodeValue) );
-            }
-        }else{
-            header_.emplace_back( std::make_pair(node, nodeValue) );
-        }
+struct Trailer{
+    std::vector<Field> trailer_;
+};
 
-        if(!foundInHeader && !foundInTrailer){
-            body_.emplace_back( std::make_pair(node, nodeValue) );
-        }
+struct FixMessage{
+    Header header_;
+    Body body_;
+    Trailer trailer_;
+};
 
-    }
+enum class FixStd : char {
+    FIX44
+};
 
+template <typename T,typename S=std::enable_if_t< std::is_convertible_v<T, std::string>, std::string> >
+[[nodiscard]] constexpr auto split(T&& str, const char delimiter) noexcept -> std::vector<S> {
 
-    auto prettyPrint(std::string_view sv) -> void{
-        
-        std::cout << "RAW " << "\n";
-        std::cout << sv << "\n\n\n";
+  auto strToConvert = std::string(std::forward<T>(str));
+  std::vector<S> internal;
 
-        std::cout << "HEADER " << "\n\n";
+  std::stringstream ss( std::forward<S>(strToConvert)); // Turn the string into a stream.
+  std::string tok;
 
-        if( header_.empty() ){
-            std::cout << "No Header" <<"\n";
-        }
+  while (getline(ss, tok, delimiter)) {
+    internal.emplace_back(tok);
+  }
 
-        for(const auto& elemHeader: header_){
+  return internal;
+}
 
-            auto [elem,value] = elemHeader;
+template<typename T,typename=std::enable_if< !std::is_integral_v<T> > >
+constexpr auto categorize(T&& vec, const FixStd fixStd) -> std::pair<FixMessage,bool> {
 
-            std::cout << elem.attribute("number").as_string() << "\t\t" << elem.attribute("name").as_string() << ": " << value << "\n"; 
-        }
+    FixMessage fixMsg;
+    Header fixHeader;
+    Body fixBody;
+    Trailer fixTrailer;
 
-        std::cout << "\n\nBODY " << "\n\n";
-
-        if( body_.empty()){
-            std::cout <<"No body"<< "\n\n";
-        }
-
-        for(const auto& elemBody: body_){
-
-            auto [elem,value] =  elemBody;
-            std::cout << elem.attribute("number").as_string() << "\t\t" << elem.attribute("name").as_string() << ": "<< value <<"\n"; 
-        }
-
-        std::cout << "\n\nTRAILER " << "\n\n"; 
-
-        if( trailer_.empty()){
-            std::cout <<"No TRAILER"<< "\n\n";
-        }
-
-
-        for(const auto& elemTrailer: trailer_){
-            auto [elem,value] = elemTrailer;
-            std::cout << elem.attribute("number").as_string() << "\t\t" << elem.attribute("name").as_string() << ": " << value << "\n"; 
-        }
-
-    }
-
-    auto fixToHuman(const FixStd fixStd, const FixMessage msg) noexcept -> void {
-
-        auto mappedVersion = [&fixStd = std::as_const(fixStd)](){
+    auto mappedVersion = [&fixStd = std::as_const(fixStd)](){
             switch (fixStd){
-            case FixStd::FIX44 :
-                return "FIX44";
-                break;
-            default:
-                return "FIX44";
-                break;
+                case FixStd::FIX44 :
+                    return "FIX44";
+                    break;
+                default:
+                    return "FIX44";
+                    break;
             }
         }();
 
+    auto currentDir = fs::current_path();
+    std::string source = currentDir;
+                source += "/spec/";
+                source += mappedVersion;
+                source += ".xml";
 
-        auto currentDir = fs::current_path();
+    pugi::xml_document fixSpec;
+    pugi::xml_parse_result result = fixSpec.load_file(source.c_str());
 
-        std::string source = currentDir;
-                    source += "/spec/";
-                    source += mappedVersion;
-                    source += ".xml";
+    if( result ){ 
 
-        pugi::xml_document fixSpec;
+        for(auto& tagValue : vec){
 
-        pugi::xml_parse_result result = fixSpec.load_file(source.c_str());
+            auto tagValueVec = split(tagValue, '=');
 
-        if( result ){ 
-            
-            auto splittedMsg = split(msg, SOH);
+            auto headers = fixSpec.child("fix").child("header");
+            auto trailers = fixSpec.child("fix").child("trailer");
             auto fields = fixSpec.child("fix").child("fields");
-            
-            for(const auto& s : splittedMsg){
+
+            auto isField = fields.find_node([&tagValueVec](auto& node){
+                return 
+                std::strcmp(node.attribute("number").as_string(), tagValueVec.front().c_str() ) == 0;
+            });
+
+            if( isField ){
+                 Field f;
+                 f.number_ = isField.attribute("number").as_int();
+                 f.value_ = tagValueVec.back();
+
+                auto nodeFoundInHeader = headers.find_node([&isField](auto& node){
+                    return 
+                    std::strcmp(node.attribute("name").as_string(), isField.attribute("name").as_string() ) == 0;
+                });
+
+                if( nodeFoundInHeader ){
+                    if( std::strcmp( nodeFoundInHeader.parent().name(), "group") ){
+                        // The node is in a <group> node
+                        f.isInGroup_ = true;
+                    }
+                    
+                    f.fieldName_ = static_cast<std::string>(nodeFoundInHeader.attribute("name").as_string());
+                    f.isComponent_ = false;
+                    f.isRequired_ = nodeFoundInHeader.attribute("required").as_int() != 0x4E ? true : false;
+
+                    fixHeader.headerFields_.emplace_back(f);
+                }
+
+                if( !nodeFoundInHeader ){
+
+                    auto nodeFoundInTrailer = trailers.find_node([&isField](auto& node){
+                        return 
+                        std::strcmp(node.attribute("name").as_string(), isField.attribute("name").as_string() ) == 0;
+                    });
+
+                    if( nodeFoundInTrailer ){
+                        f.fieldName_ = static_cast<std::string>(nodeFoundInTrailer.attribute("name").as_string());
+                        f.isComponent_ = false;
+                        f.isInGroup_ = false;
+                        f.isRequired_ = nodeFoundInTrailer.attribute("required").as_int() != 0x4E ? true : false;
+
+                        fixTrailer.trailer_.emplace_back( f );
+                    }else{
+
+                        // The node is in the body in this case so we construct a Tag object
+                        // The question about the difference between Tag and Field may arise as this point
+                        Tag tag ;
+
+                        tag.name_ = static_cast<std::string>( isField.attribute("name").as_string() );
+                        tag.number_ = isField.attribute("number").as_int();
+                        tag.type_ = static_cast<std::string>( isField.attribute("type").as_string() );
+                        tag.value_ = tagValueVec.back();
+                        // If the field has some set of values we retrieve them
+
+                        for( const auto& value: isField.children() ){
+                            Value v;
+                            v.description_ = static_cast<std::string>( value.attribute("type").as_string() );
+                            v.enumValue_  = static_cast<std::string>( value.attribute("enum").as_string() );
+
+                            tag.tagValues_.emplace_back( v );
+                        }
+
+                        fixBody.tagValues_.emplace_back( tag );
+                    }
+
+                }
+
+            }else{
                 
-                 auto attrs = split(s,'=');
-                 auto foundElem = fields.find_child_by_attribute("field","number",attrs.front().c_str());
+                // The field is not a correct field, means we didn't found an attribute number=x
+                // This results in parsing error
 
-                 if( foundElem ){
-                     categorize(foundElem, attrs.back(), fixSpec);
-                 }else{
-                     std::cout << "Element with Tag="<< attrs.front() << " Not found" << "\n";
-                 }
+                std::cout << "Field with tag="<< tagValueVec.front() << " Not found\n";
             }
-
-            prettyPrint(msg);
-
-        }else{
-            std::cout << "An error occured: ";
-            std::cout << result.description() << "\n";
+ 
         }
 
+        fixMsg.header_ = fixHeader;
+        fixMsg.body_ = fixBody;
+        fixMsg.trailer_ = fixTrailer;
+
+        return std::make_pair(fixMsg, false);
+    }else{
+
+        std::cerr << "Cannot open the FIX spec file" <<"\n";
+        return { FixMessage{}, true};
+    }
+}
+
+/**
+ * @brief Check the message validity
+ * @return true if the body length is correct false otherwise
+*/
+template <typename T,typename=std::enable_if_t<std::is_convertible_v<T,std::string> > >
+constexpr auto checkMsgValidity(T&& message) noexcept -> bool {
+
+    auto splittedMsg = split( std::forward<T>(message), SOH);
+
+    auto [fixMessage, error] = categorize( splittedMsg, FixStd::FIX44);
+ 
+    prettyPrint( fixMessage );
+   
+    if( !error ){
+         std::cout << "Possibly no errors found :) \n";
+
+         prettyPrint( fixMessage );
+    }else{
+       std::cout << "Parsing ends with errors"<< "\n";
     }
 
+    return error;
+
+}
 
 }// namespace fixparser
